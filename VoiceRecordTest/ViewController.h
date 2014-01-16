ccc62756c508610fd2bae45a7cdb6e66d8e5a0ea
SDT1 <eyebeam79@naver.com>
2014-01-16 14:43:31 +0900

    Initial Commit

---
+++ VoiceRecordTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VoiceRecordTest
+//
+//  Created by SDT1 on 2014. 1. 16..
+//  Copyright (c) 2014년 SDT1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
